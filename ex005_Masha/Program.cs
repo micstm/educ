d882a41bf31b51241f16aaa6b06882d2d1399fa7
mic <mic_st@mail.ru>
2022-10-05 01:16:45 +0400

    add ex005 Hi, Masha

---
+++ ex005_Masha/Program.cs
@@ -0,0 +1,14 @@
+﻿// привет, Маша
+
+Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine() ?? "";
+
+if(userName.ToLower() == "маша")
+{
+    Console.WriteLine("УРА! Это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
