7358ce768b09c79c069506b9c32294c941d3c384
mic <mic_st@mail.ru>
2022-10-04 17:29:00 +0400

    + DZ012

--- dz012/Program.cs
+++ dz012/Program.cs
@@ -1,2 +1,32 @@
 ﻿//на вход три числа и выдаёт максимальное
-Console.WriteLine("Hello, World!");
+
+int max;
+
+Console.Write("Введите первое целое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+
+Console.Write("Введите второе целое число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int userNumber2 = int.Parse(userInput2);
+
+Console.Write("Введите третье целое число: ");
+string userInput3 = Console.ReadLine() ?? "";
+int userNumber3 = int.Parse(userInput3);
+
+if (userNumber1 > userNumber2)
+    {
+        max = userNumber1;
+    }
+    else
+    {
+        max = userNumber2;
+    }
+
+if (userNumber3 > max)
+    {
+        max = userNumber3;
+    }
+
+Console.WriteLine("a= " + userNumber1 + ", b= " + userNumber2 + ", c= " + userNumber3 + " -> max= " + max);
+
