354563658b1f19d3ad7e1b11f19cd0df587a6b83
mic <mic_st@mail.ru>
2022-10-04 18:52:24 +0400

    + DZ014

---
+++ dz014/Program.cs
@@ -0,0 +1,12 @@
+﻿//показывает все чётные числа от 1 до N
+
+Console.Write("Введите целое число :");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+
+int N = 2;
+Console.Write(userNumber + " => ");
+while (N <= userNumber) {
+        Console.Write(N + ", ");
+        N+=2;
+    }
