98bf0def2ee701bbecf1fc65575615d19bb0f0eb
mic <mic_st@mail.ru>
2022-10-04 17:46:32 +0400

    + DZ013

---
+++ dz013/Program.cs
@@ -0,0 +1,14 @@
+﻿// является ли число чётным
+
+Console.Write("Введите целое число :");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+
+if (userNumber%2 == 0)
+    {
+        Console.WriteLine(userNumber + " => чётное");
+    }
+    else
+        {
+            Console.WriteLine(userNumber + " => нечётное");
+        }
